d1983b648608c8c8d6da51da890202b2a812f3fe
Sakke <santsukan@gmail.com>
2020-11-05 17:50:04 +0200

    Add test directories and basis for arithmetic tests

---
+++ test/arithmetic.test.js
@@ -0,0 +1,7 @@
+import chai from "chai"
+import add from "../src/add.js"
+import ceil from "../src/ceil.js"
+import clamp from "../src/clamp.js"
+import divide from "../src/divide.js"
+
+// Arithmetic tests will be here
