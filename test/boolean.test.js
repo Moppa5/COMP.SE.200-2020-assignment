a57ba7860752099c65e7370166540ab76b690c1b
Henrik Kaakkolammi <henrix1998@gmail.com>
2020-11-12 15:47:40 +0200

    Initial boolean tests

--- test/boolean.test.js
+++ test/boolean.test.js
@@ -1,6 +1,8 @@
 import chai from "chai"
+const expect = chai.expect
 
 // Boolean category modules
+import add from "../src/add.js"
 import eq from "../src/eq.js"
 import isArgument from "../src/isArguments.js"
 import isArrayLike from "../src/isArrayLike.js"
@@ -14,3 +16,76 @@ import isObject from "../src/isObject.js"
 import isObjectLike from "../src/isObjectLike.js"
 import isSymbol from "../src/isSymbol.js"
 import isTypedArray from "../src/isTypedArray.js"
+
+const modules = {
+    functions: [
+		isArgument,
+		isArrayLike,
+		isArrayLikeObject,
+		isBoolean,
+		isBuffer,
+		isDate,
+		isEmpty,
+		isLength,
+		isObject,
+		isObjectLike,
+		isSymbol,
+		isTypedArray
+    ],
+    params: [
+		{ parameter: function() { return arguments}(), description: "Function arguments"},
+		{ parameter: [-1, 0, 1], description: "Numeric array"},
+		{ parameter: "abc", description: "String"},
+		{ parameter: true, description: "True"},
+		{ parameter: false, description: "False"},
+		{ parameter: new Buffer(8), description: "Buffer(8)"},
+		{ parameter: new Date, description: "New Date"},
+		{ parameter: "11/9/2001", description: "Datelike string 11/9/2001"},
+		{ parameter: null, description: "Null"},
+		{ parameter: 1, description: "Numeric 1"},
+		{ parameter: {"key": "value"}, description: "Basic object with one key and value"},
+		{ parameter: Number.MAX_VALUE, description: "Number.MAX_VALUE"},
+		{ parameter: Infinity, description: "Infinity"},
+		{ parameter: "4", description: "String 4"},
+		{ parameter: {}, description: "Empty object"},
+		{ parameter: add, description: "Function"},
+		{ parameter: Symbol.iterator, description: "Symbol"},
+		{ parameter: new Float32Array, description: "Empty float array"},
+		{ parameter: [], description: "Empty typeless array"}
+	],
+	results: [
+		[true,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false],
+		[false,true,true,false,false,false,false,true,false,false,false,false,false,true,false,false,false,true,true],
+		[false,true,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,true,true],
+		[false,false,false,true,true,false,false,false,false,false,false,false,false,false,false,false,false,false,false],
+		[false,false,false,false,false,true,false,false,false,false,false,false,false,false,false,false,false,false,false],
+		[false,false,false,false,false,false,true,false,false,false,false,false,false,false,false,false,false,false,false],
+		[false,false,false,true,true,false,false,false,true,true,false,false,false,false,true,false,false,false,true],
+		[false,false,false,false,false,false,false,false,false,true,false,false,false,false,false,false,false,false,false],
+		[true,true,true,true,true,true,true,true,false,true,true,true,true,true,true,true,true,true,true],
+		[true,true,true,true,true,true,true,true,false,true,true,true,true,true,true,false,true,true,true],
+		[false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,true,false,false],
+		[false,true,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,true,false]
+	]
+}
+
+function runTests() {
+	for(let f=0; f<modules.functions.length; f++){
+		let testedfunc = modules.functions[f]
+
+		describe(testedfunc.name, ()=> {
+
+			for (let p=0; p<modules.params.length; p++) {   
+				let testedparam = modules.params[p]
+				let result = modules.results[f][p]
+
+				it(testedparam.description + ", expected to be " + result, () => {
+					expect(testedfunc(testedparam.parameter)).to.equal(result)
+				})
+			}
+		})
+	}
+}
+
+//console.log(modules.results[0][0])
+runTests()
