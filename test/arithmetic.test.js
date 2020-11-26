44559b30b8b5d75345e999a8d19e90cb9b94f18b
Sakke <santsukan@gmail.com>
2020-11-26 18:02:40 +0200

    Re-define runTests() to run all tests by given testProcess

--- test/arithmetic.test.js
+++ test/arithmetic.test.js
@@ -21,6 +21,7 @@ const test = {
 const modules = {
     ADD: {
         DESCRIPTION: "Add",
+        func: add,
         MODEL: new Map([
             [test.ADD_BASIC, [ {p1: 25, p2: 4}, {p1: 45, p2: -2} ] ],
             [test.LIMIT, [ {p1: Infinity, p2: 2}, {p1: Infinity, p2: Infinity} ] ]
@@ -32,6 +33,7 @@ const modules = {
     }, 
     DIVIDE: {
         DESCRIPTION: "Divide",
+        func: divide,
         MODEL: new Map([
             [test.DIV_BASIC, [ {p1: 28, p2: 4}, {p1: 44, p2: -2} ] ],
             [test.LIMIT, [ {p1: 2, p2: Infinity}, {p1: Infinity, p2: 3} ] ]
@@ -43,6 +45,7 @@ const modules = {
     }, 
     CLAMP: {
         DESCRIPTION: "Clamp",
+        func: clamp,
         MODEL: new Map([
             [test.CLAMP_BASIC, [ {p1: -10, p2: -5, p3: 5}, {p1: 10, p2: -5, p3: 5} ] ]
         ]),
@@ -52,6 +55,7 @@ const modules = {
     }, 
     CEIL: {
         DESCRIPTION: "Ceil",
+        func: ceil,
         MODEL: new Map([
             [test.CEIL_BASIC, [ {p1: 4.006, p2: 0}, {p1: 6.004, p2: 2}, 
                 {p1: 6040, p2: -2}, {p1: 4.99, p2: 0}, {p1: 4, p2: 0} ] ],
@@ -64,36 +68,43 @@ const modules = {
     }
 };
 
+const testProcess = [modules.ADD,
+                     modules.DIVIDE,
+                     modules.CLAMP,
+                     modules.CEIL,
+                    ]
+
 // Execute test
-function runTests(module, moduleFunc) {
+function runTests() {
 
-    describe(module.DESCRIPTION, ()=> {
-        for (let testCase of module.MODEL.keys()) {    
-            it(testCase, () => {
-                let currentDataArray = module.MODEL.get(testCase)
-                let expectedData = module.EXPECTED.get(testCase)
-                let dataLength = currentDataArray.length
+    for (let i in testProcess) {
+        let module = testProcess[i]
 
-                for (let pos = 0; pos < dataLength; pos++ ) {
+        describe(module.DESCRIPTION, ()=> {
+            for (let testCase of module.MODEL.keys()) {    
+                it(testCase, () => {
+                    let currentDataArray = module.MODEL.get(testCase)
+                    let expectedData = module.EXPECTED.get(testCase)
+                    let dataLength = currentDataArray.length
 
-                    if (module.DESCRIPTION == modules.CLAMP.DESCRIPTION) {
-                        // Clamp has 3 parameters
-                        expect( moduleFunc(currentDataArray[pos].p1,
-                            currentDataArray[pos].p2, 
-                            currentDataArray[pos].p3) ).
-                            to.equal(expectedData[pos])
-                    } else {
-                        // Other modules have either 2 or 1
-                        expect( moduleFunc(currentDataArray[pos].p1,
-                            currentDataArray[pos].p2)).to.equal(expectedData[pos])
+                    for (let pos = 0; pos < dataLength; pos++ ) {
+
+                        if (module.DESCRIPTION == modules.CLAMP.DESCRIPTION) {
+                            // Clamp has 3 parameters
+                            expect( module.func(currentDataArray[pos].p1,
+                                currentDataArray[pos].p2, 
+                                currentDataArray[pos].p3) ).
+                                to.equal(expectedData[pos])
+                        } else {
+                            // Other modules have either 2 or 1
+                            expect( module.func(currentDataArray[pos].p1,
+                                currentDataArray[pos].p2)).to.equal(expectedData[pos])
+                        }
                     }
-                }
-            })
-        }
-    })
+                })
+            }
+        })
+    }
 }
 
-runTests(modules.ADD, add)
-runTests(modules.DIVIDE, divide)
-runTests(modules.CLAMP, clamp)
-runTests(modules.CEIL, ceil)
+runTests()
