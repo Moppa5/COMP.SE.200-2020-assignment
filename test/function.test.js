6e607e3b459929ac73a432ba23d9d4ea4a78dc36
Sakke <santsukan@gmail.com>
2020-11-29 11:12:15 +0200

    Implement function category tests (Related: #1)

--- test/function.test.js
+++ test/function.test.js
@@ -1,5 +1,60 @@
 import chai from "chai"
+const expect = chai.expect
 
-// Function category modules
+// Function category modules. Reduce is exluced from function
 import memoize from "../src/memoize.js"
-// Note: reduce might fit on this category too. Now on array.test.js
+
+// Test related data
+const test = {
+    DESCRIPTION: "Memoize",
+    BASIC: "Module's own examples",
+    OTHER: "Change memoized data",
+    THROW: "Pass a non-func parameter",
+    THROW_RES: "Pass a non-func resolver"
+}
+
+// Return object values. For memoize
+function testFunction(object) {
+    return Object.values(object)
+}
+
+// Test suite as basic mocha and chai since only one module
+describe(test.DESCRIPTION, ()=> {
+    const object = { 'a': 1, 'b': 2 }
+    const other = { 'c': 3, 'd': 4 }
+
+    it(test.BASIC, ()=> {
+        const test = memoize(testFunction)
+        expect( test(object) ).to.deep.equal([1, 2])
+        expect( test(other) ).to.deep.equal([3, 4])
+    })
+
+    it(test.OTHER, ()=> {
+        const test = memoize(testFunction)
+        expect( test(object) ).to.deep.equal([1, 2]) 
+        object.a = 5 // Change shouldn't affect memoize since it's cached
+
+        expect( test(object) ).to.deep.equal([1, 2])
+        object.a = 2 // change back to normal
+
+        test.cache.set(object, [3, 4]) // changing cache should affect
+        expect( test(object) ).to.deep.equal([3, 4])
+    })
+
+    it(test.THROW, ()=> {
+        expect( () =>  memoize("testString") ).to.throw(TypeError)
+        expect( () => memoize(null) ).to.throw(TypeError)
+        expect( () => memoize(Infinity) ).to.throw(TypeError)
+        expect( () => memoize(NaN) ).to.throw(TypeError)
+        expect( () => memoize(5) ).to.throw(TypeError)
+    })
+
+    it (test.THROW_RES, ()=> {
+        const test = memoize(testFunction)
+        expect( () =>  memoize(test(object), null) ).to.throw(TypeError)
+        expect( () =>  memoize(test(object), Infinity) ).to.throw(TypeError)
+        expect( () =>  memoize(test(object), "testString") ).to.throw(TypeError)
+        expect( () =>  memoize(test(object), 2) ).to.throw(TypeError)
+    })
+
+})
