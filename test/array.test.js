5cfceed86fa99d794e5bbeeef45de6d4d5a33e59
Henrik Kaakkolammi <henrix1998@gmail.com>
2020-12-05 15:44:27 +0200

    Add array and object tests, further fix to travis

--- test/array.test.js
+++ test/array.test.js
@@ -1,14 +1,245 @@
 import chai from "chai"
+const expect = chai.expect
 
 // Array category modules
 // castArray static tested
-import at from "../src/at.js"
 import chunk from "../src/chunk.js"
 import compact from "../src/compact.js"
-import countBy from "../src/countBy.js"
 import difference from "../src/difference.js"
 import drop from "../src/drop.js"
 import every from "../src/every.js"
 import filter from "../src/filter.js"
 import map from "../src/map.js"
 import reduce from "../src/reduce.js" // Note: Reduce belongs also to function category
+
+const testArrays = {
+    NUMBER: [0, 1, 2, -1, 10, 0, 40, -10, -11, 9, -10, 5],
+    STRING: ["a", "5.", "c", "d", "es", "nolla", "40", "es", "asd", "sana", "es", "5."],
+    MIXED: [0, false, true, NaN, "stringa", "90", Infinity],
+    DIFFERENCE: [0, -10, 5, 3, 4, NaN, false, Infinity, "90", 95]
+}
+
+function runTests() {
+    testfChunk()
+	testfCompact()
+    testfDifference()
+    testfDrop()
+    testfEvery()
+    testfFilter()
+    testfMap()
+    testfReduce()
+}
+
+function testfChunk(){
+    describe("chunk", ()=> {
+        it("Number array: Array divided even", () => {
+            expect(chunk(testArrays.NUMBER, 2)).to.eql(
+                [[0, 1], [2, -1], [10, 0], [40, -10], [-11, 9], [-10, 5]]
+            )
+        }),
+        it("String array: Array not divided even", () => {
+            expect(
+                chunk(testArrays.STRING, 5)).to.eql(
+                [[0, 1, 2, -1, 10], [0, 40, -10, -11, 9], [-10, 5]]
+            )
+        }),
+        it("Empty array", () => {
+            expect(
+                chunk([], 5)).to.eql(
+                []
+            )
+        }),
+        // Bit philosophical
+        it("Chunk by zero", () => {
+            expect(
+                chunk(testArrays.NUMBER, 0)).to.eql(
+                []
+            )
+        })
+    })
+}
+
+function testfCompact(){
+    // This behaves in a very weird way
+    describe("compact", ()=> {
+        it("No falsey values", () => {
+            expect(compact(testArrays.STRING))
+                .to.eql(
+                    testArrays.STRING
+                )
+        }),
+        it("Falsey values", () => {
+            expect(compact(
+                    [true, false, null, 0, "", undefined, NaN, 1, "nice"]))
+                .to.eql(
+                    [true, 1, "nice"]
+                )
+        })
+    })
+}
+
+function testfDifference(){
+    describe("difference", ()=> {
+        it("Number array", () => {
+            expect(difference(testArrays.NUMBER, testArrays.DIFFERENCE))
+                .to.eql(
+                    [1, 2, -1, 10, 40, -11, 9]
+                )
+        }),
+        it("Mixed array", () => {
+            expect(difference(testArrays.MIXED, testArrays.DIFFERENCE))
+                .to.eql(
+                    [true, "stringa"]
+                )
+        }),
+        it("Empty inputs", () => {
+            expect(difference([], []))
+                .to.eql(
+                    []
+                )
+        })
+    })
+}
+
+function testfDrop(){
+    describe("drop", ()=> {
+        it("Drop 0", () => {
+            expect(drop(testArrays.NUMBER, 0))
+                .to.eql(
+                    testArrays.NUMBER
+                )
+        }),
+        it("Drop n>array.length", () => {
+            expect(drop(testArrays.STRING, 20))
+                .to.eql(
+                    []
+                )
+        }),
+        it("Drop 0<n<array.length", () => {
+            expect(drop(testArrays.MIXED, 3))
+                .to.eql(
+                    [NaN, "stringa", "90", Infinity]
+                )
+        }),
+        it("Empty input", () => {
+            expect(drop([], 20))
+                .to.eql(
+                    []
+                )
+        })
+    })
+}
+
+function testfEvery() {
+    var predicate = (value, index, array) =>{
+        return (value < 100)
+    }
+
+    describe("every", ()=> {
+        it("All true", () => {
+            expect(every(testArrays.NUMBER, predicate))
+                .to.equal(
+                    true
+                )
+        }),
+        it("Some false", () => {
+            expect(every(testArrays.DIFFERENCE, predicate))
+                .to.equal(
+                    false
+                )
+        }),
+        it("Empty input", () => {
+            expect(every([], predicate))
+                .to.equal(
+                    true
+                )
+        })
+    })
+}
+
+function testfFilter() {
+    var predicate = (value, index, array) =>{
+        return (value < 80 && index < 10)
+    }
+
+    describe("filter", ()=> {
+        it("All true", () => {
+            expect(filter(testArrays.NUMBER, predicate))
+                .to.eql(
+                    [0, 1, 2, -1, 10, 0, 40, -10, -11, 9]
+                )
+        }),
+        it("Some false", () => {
+            expect(filter(testArrays.DIFFERENCE, predicate))
+                .to.eql(
+                    [0, -10, 5, 3, 4, false]
+                )
+        }),
+        it("Empty input", () => {
+            expect(filter([], predicate))
+                .to.eql(
+                    []
+                )
+        })
+    })
+}
+
+function testfMap() {
+    var iteratee = (value, index, array) =>{
+        return (value + 5 + index)
+    }
+
+    describe("map", ()=> {
+        it("Positive case", () => {
+            expect(map(testArrays.NUMBER, iteratee))
+                .to.eql(
+                    [5, 7, 9, 7, 19, 10, 51, 2, 2, 23, 5, 21]
+                )
+        }),
+        it("Empty input", () => {
+            expect(map([], iteratee))
+                .to.eql(
+                    []
+                )
+        })
+    })
+}
+
+function testfReduce(){
+    var iteratee = (accumulator, value, index, collection) =>{
+        return (accumulator + value + index)
+    }
+
+    describe("reduce", ()=> {
+        it("Number input", () => {
+            expect(reduce(testArrays.NUMBER, iteratee))
+                .to.equal(
+                    35+66   // elements+indexes
+                )
+        }),
+        it("Empty input with initial accumulator", () => {
+            expect(reduce([], iteratee, 0))
+                .to.eql(
+                    0
+                )
+		}),
+		// NOTE: maybe should throw error instead of returning undefined
+		it("Empty input without initial accumulator", () => {
+            expect(reduce([], iteratee))
+                .to.eql(
+                    undefined
+                )
+        }),
+        it("Example test case", () => {
+            expect(reduce({ 'a': 1, 'b': 2, 'c': 1 }, (result, value, key) => {
+                (result[value] || (result[value] = [])).push(key)
+                return result
+              }, {}))
+                .to.eql(
+                    { '1': ['a', 'c'], '2': ['b'] }
+                )
+        })
+    })
+}
+
+runTests()
--- test/object.test.js
+++ test/object.test.js
@@ -1,5 +1,165 @@
 import chai from "chai"
+const expect = chai.expect
 
 // object category modules
+import at from "../src/at.js"
+import countBy from "../src/countBy.js"
 import get from "../src/get.js"
 import keys from "../src/keys.js"
+
+const testObjects = {
+    OBJECT: {
+		'a': [
+			{'e': 2},
+			{'f': 3},
+			{'g': 4}
+		],
+		'b': 5,
+		'c': 6,
+		'd': 7,
+	},
+	LIST: [
+		{'name': 'cat', 'animal': true},
+		{'name': 'table', 'animal': false},
+		{'name': 'dog', 'animal': true},
+		{'name': 'shoe', 'animal': false},
+		{'name': 'pc', 'animal': false}
+	]
+}
+
+// This is for keys
+function testfunc(){
+	this.first = 2
+	this.second = 3
+}
+
+function runTests() {
+	testfAt()
+	testfCountBy()
+	testfGet()
+	testfKeys()
+}
+
+function testfAt(){
+	describe("at", ()=> {
+        it("Pick multiple values", () => {
+            expect(at(testObjects.OBJECT, ['a[0].e', 'a[1]', 'b', 'c']))
+                .to.eql(
+                    [2, {'f': 3}, 5, 6]
+                )
+		}),
+		// NOTE: maybe should throw error instead of returning undefined
+		it("Incorrect path", () => {
+            expect(at(testObjects.OBJECT, ['a[0].h', 'a[2].g']))
+                .to.eql(
+                    [undefined, 4]
+                )
+		}),
+		it("Empty input object and paths", () => {
+            expect(at({}, []))
+                .to.eql(
+                    []
+                )
+        })
+    })
+}
+
+function testfCountBy(){
+	var testarray = [0, 1, 2, 2, 1, 2, 10]
+
+    describe("countBy", ()=> {
+        it("Array of objects", () => {
+            expect(countBy(testObjects.LIST, value => value.animal))
+                .to.eql(
+                    { 'true': 2, 'false': 3 }
+                )
+		}),
+		it("Example test case", () => {
+			expect(countBy([{ 'user': 'barney', 'active': true },
+							{ 'user': 'betty', 'active': true },
+							{ 'user': 'fred', 'active': false }], 
+							value => value.active))
+                .to.eql(
+                    { 'true': 2, 'false': 1 }
+                )
+		}),
+		it("Array input", () => {
+            expect(countBy(testarray, value => value))
+                .to.eql(
+                    { 0: 1, 1: 2, 2: 3, 10: 1}
+                )
+		}),
+		it("Empty array", () => {
+            expect(countBy([], value => value.animal))
+                .to.eql(
+                    {}
+                )
+        })
+    })
+}
+
+function testfGet(){
+	describe("get", ()=> {
+        it("Positive case, shallow", () => {
+            expect(at(testObjects.OBJECT, 'b'))
+                .to.equal(
+                    5
+                )
+		}),
+		it("Positive case, deep", () => {
+            expect(get(testObjects.OBJECT, ['a', 0, 'e']))
+                .to.equal(
+                    2
+                )
+		}),
+		it("Incorrect path", () => {
+            expect(get(testObjects.OBJECT, 'a[0].h', "this is default value"))
+                .to.equal(
+                    "this is default value"
+                )
+		}),
+		it("Empty input object and path", () => {
+            expect(get({}, ''))
+                .to.equal(
+                    undefined
+                )
+        })
+    })
+}
+
+function testfKeys(){
+	describe("keys", ()=> {
+        it("Object input", () => {
+            expect(keys(testObjects.OBJECT))
+                .to.eql(
+                    ['a', 'b', 'c', 'd']
+                )
+		}),
+		it("Function input", () => {
+            expect(keys(new testfunc))
+                .to.eql(
+                    ["first", "second"]
+                )
+		}),
+		it("String input", () => {
+            expect(keys("test"))
+                .to.eql(
+                    ['0', '1', '2', '3']
+                )
+		}),
+		it("Array input", () => {
+            expect(keys([5,6,7]))
+                .to.eql(
+                    ['0', '1', '2']
+                )
+		}),
+		it("Empty input", () => {
+            expect(keys({}))
+                .to.eql(
+                    []
+                )
+		})
+    })
+}
+
+runTests()
